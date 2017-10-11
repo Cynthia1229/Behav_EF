4d19a5e583a67a5205ec5d1ff673502650d7d85c
Cynthia1229 <32211750+Cynthia1229@users.noreply.github.com>
2017-10-11 22:06:46 +0800

    parameter analysis (#6)
    * parameter analysis
    
    * parameter analysis
    
    * remove false code
    
    CateSwitch, ShiftColor, ShiftNumber

---
+++ analysisAntiSac.m
@@ -0,0 +1,20 @@
+
+clear
+DATAFOLDER = 'EFMerge';
+KEYMETAVAR = {'id', 'time'};
+ANADATAVAR = {'Score'};
+
+data = readtable(fullfile(DATAFOLDER, 'AntiSac.csv'));
+[grps, gid] = findgroups(data(:, KEYMETAVAR));
+[stats, labels] = splitapply(@antisac, data(:, ANADATAVAR), grps);
+results = [gid, array2table(stats, 'VariableNames', labels(1, :))];
+writetable(results, fullfile(DATAFOLDER, 'analysisAntiSac.csv'))
+
+function [stats, labels] = antisac(score)
+
+NTrial = length(score);
+ACC = sum(score)/NTrial;
+stats = [NTrial, ACC];
+labels = {'NTrial', 'ACC'};
+
+end
