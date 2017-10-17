54df0213b58539a413904995f61d0753fbf888b5
Cynthia1229 <jintaosheng1229@gmail.com>
2017-10-17 22:33:56 +0800

    done
    stroop is done.

--- analysisStroop.m
+++ analysisStroop.m
@@ -2,30 +2,17 @@
 clear
 DATAFOLDER = 'EFMerge';
 KEYMETAVAR = {'id', 'time'};
-ANADATAVAR = {'Material', 'Unknown', 'RT', 'KeyResponse'};
+ANADATAVAR = {'condition', 'RT', 'KeyResponse'};
 
 data = readtable(fullfile(DATAFOLDER, 'Stroop.csv'));
+data(isnan(data.id), :) = [];
+data.condition = categorical ((strncmp (data.Material, 'r', 1)) & (data.Color == 32418) | ...
+    (strncmp (data.Material, 'g', 1)) & (data.Color == 32511) | ...
+    (strncmp (data.Material, 'b', 1)) & (data.Color == 34013) | ...
+    (strncmp (data.Material, 'y', 1)) & (data.Color == 40644) == 1, [true, false], {'Cong', 'Incong'});
+
+data.RT = data.RT * 1000;
 [grps, gid] = findgroups(data(:, KEYMETAVAR));
-[stats, labels] = splitapply(@stroop, data(:, ANADATAVAR), grps);
+[stats, labels] = splitapply(@sngprocControl, data(:, ANADATAVAR), grps);
 results = [gid, array2table(stats, 'VariableNames', labels(1, :))];
 writetable(results, fullfile(DATAFOLDER, 'analysisStroop.csv'))
-
-function [stats, labels] = stroop(material, color, rt, acc)
-
-NTrial = length(material);
-NResp = sum(~isnan(acc));
-
-cond = categorical ((strncmp (material, 'r', 1)) & (color == 32418) | ...
-    (strncmp (material, 'g', 1)) & (color == 32511) | ...
-    (strncmp (material, 'b', 1)) & (color == 34013) | ...
-    (strncmp (material, 'y', 1)) & (color == 40644) == 1, [true, false], {'Cong', 'Incong'});
-rt = rmoutlier(rt * 1000);
-acc(isnan(rt)) = nan;
-NInclude = sum(~isnan(acc));
-mRT_Cong = nanmean(rt(acc == 1 & cond == 'Cong'));
-mRT_Incong = nanmean(rt(acc == 1 & cond == 'Incong'));
-mRT_Incon_Con = mRT_Incong - mRT_Cong;
-stats = [NTrial, NResp, NInclude, mRT_Cong, mRT_Incong, mRT_Incon_Con];
-labels = {'NTrial', 'NResp', 'NInclude', 'mRT_Cong', 'mRT_Incong', 'mRT_Incon_Con'};
-
-end
