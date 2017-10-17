d47ff9bc959470c6cef5d338eb6f4a629bc50d6a
Cynthia1229 <32211750+Cynthia1229@users.noreply.github.com>
2017-10-17 23:36:21 +0800

    save results (#9)
    * parameter analysis
    
    * parameter analysis
    
    * remove false code
    
    CateSwitch, ShiftColor, ShiftNumber
    
    * ShiftingNumber and Stroop
    
    please check them.
    
    * done
    
    stroop is done.
    
    * save results
    
    done

--- analysisShiftingColor.m
+++ analysisShiftingColor.m
@@ -13,3 +13,4 @@ data.RT = data.RT * 1000;
 [grps, gid] = findgroups(data(:, KEYMETAVAR));
 [stats, labels] = splitapply(@sngprocControl, data(:, ANADATAVAR), grps);
 results = [gid, array2table(stats, 'VariableNames', labels(1, :))];
+writetable(results, fullfile(DATAFOLDER, 'ShiftColorResult.csv'))
