bf9bfe9e681ec5633151a692e5c1ff1e1c234aac
Zhang Liang <psychelzh@gmail.com>
2017-10-18 23:43:11 +0800

    FIX BUG: fixed issues of treating missings in 'AntiSac'.

--- analysisAntiSac.m
+++ analysisAntiSac.m
@@ -2,19 +2,27 @@
 clear
 DATAFOLDER = 'EFMerge';
 KEYMETAVAR = {'id', 'time'};
-ANADATAVAR = {'Score'};
+ANADATAVAR = {'RT', 'Score'};
 
 data = readtable(fullfile(DATAFOLDER, 'AntiSac.csv'));
+data.Score(data.keySelect == 0) = -1;
 [grps, gid] = findgroups(data(:, KEYMETAVAR));
 [stats, labels] = splitapply(@antisac, data(:, ANADATAVAR), grps);
 results = [gid, array2table(stats, 'VariableNames', labels(1, :))];
 writetable(results, fullfile(DATAFOLDER, 'AntiSacResult.csv'))
 
-function [stats, labels] = antisac(score)
+function [stats, labels] = antisac(rt, acc)
 
-NTrial = length(score);
-ACC = sum(score)/NTrial;
-stats = [NTrial, ACC];
-labels = {'NTrial', 'ACC'};
+NTrial = length(acc);
+NResp = sum(acc ~= -1);
+% set rt of no response trials as NaN
+rt(acc == -1) = nan;
+rt = rmoutlier(rt * 1000);
+acc(isnan(rt)) = nan;
+NInclude = sum(~isnan(acc));
+MRT = nanmean(rt(acc == 1));
+PE = 1 - nanmean(acc);
+stats = [NTrial, NResp, NInclude, MRT, PE];
+labels = {'NTrial', 'NResp', 'NInclude', 'MRT', 'PE'};
 
 end
