76cd78faf5701a253e23f7783e7a6157af8184be
Zhang Liang <psychelzh@gmail.com>
2017-10-19 23:05:42 +0800

    FIX BUG: include no response trials but remove too-quick trials 'AntiSac'.

--- analysisAntiSac.m
+++ analysisAntiSac.m
@@ -5,7 +5,6 @@ KEYMETAVAR = {'id', 'time'};
 ANADATAVAR = {'RT', 'Score'};
 
 data = readtable(fullfile(DATAFOLDER, 'AntiSac.csv'));
-data.Score(data.keySelect == 0) = -1;
 [grps, gid] = findgroups(data(:, KEYMETAVAR));
 [stats, labels] = splitapply(@antisac, data(:, ANADATAVAR), grps);
 results = [gid, array2table(stats, 'VariableNames', labels(1, :))];
@@ -14,14 +13,12 @@ writetable(results, fullfile(DATAFOLDER, 'AntiSacResult.csv'))
 function [stats, labels] = antisac(rt, acc)
 
 NTrial = length(acc);
-NResp = sum(acc ~= -1);
+NResp = sum(rt ~= 0);
 % set rt of no response trials as NaN
-rt(acc == -1) = nan;
-rt = rmoutlier(rt * 1000);
-acc(isnan(rt)) = nan;
-NInclude = sum(~isnan(acc));
-MRT = nanmean(rt(acc == 1));
-PE = 1 - nanmean(acc);
+acc(outlier(rt, 'Method', 'cutoff', 'Boundary', [100, inf])) = -1;
+NInclude = sum(acc ~= -1);
+MRT = mean(rt(acc == 1));
+PE = 1 - sum(acc == 1) / NInclude;
 stats = [NTrial, NResp, NInclude, MRT, PE];
 labels = {'NTrial', 'NResp', 'NInclude', 'MRT', 'PE'};
 
